02e827b7f58ca0cfaf0ab1c646b32bfa58254a67
apfelix1 <fanfei.002@gmail.com>
2021-01-26 17:08:51 +0800

    script

--- execution.py
+++ execution.py
@@ -1,6 +1,34 @@
-from higgsboom.MarketData.CSecurityMarketDataUtils import *
+import urllib
+import urllib.request
+import urllib
+import time
+from higgsboom.FuncUtils.DateTime import *
 
-secUtils = CSecurityMarketDataUtils('Z:/StockData')
-df = secUtils.FundTAQDataFrame('515580.SH', '20200714')
-df.to_csv('.\\test.csv')
-print(secUtils.FundTAQDataFrame('515580.SH', '20200714'))
+
+# 抓取页面方法，调用该方法返回抓取到数据
+def read_pageHtml(url):
+    file = urllib.request.Request(url,headers={"Upgrade-Insecure-Requests":1, "User-Agent" : "agod/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"} )
+    file = urllib.request.urlopen(url)
+    data = file.read()
+    return data
+
+
+# 将数据生成txt文件方法 传入保存文件路径 storagePath 以及文件数据 data
+def storageToLocalFiles(storagePath, data):
+    fhandle = open(storagePath, "wb")
+    fhandle.write(data)
+    fhandle.close()
+
+tdPeriodList = TradingDays(startDate='20200312', endDate='20201231')
+
+for i in tdPeriodList:
+    print(i)
+    i = i.replace('-','')
+    url = "http://query2.efunds.com.cn/data/ETFEFundBulletin"+i+"_510310.txt"
+    print(url)
+    data = read_pageHtml(url)
+    storagePath = ".\\tradelist\\510310\\"+'510310'+i+".txt"
+    storageToLocalFiles(storagePath, data)
+    time.sleep(3)
+
+
